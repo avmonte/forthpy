606278bd77b976ba208e34336e5c0d00d657db39
avmonte <unstoppablehay@gmail.com>
2022-12-13 01:41:32 +0400

    testing command differentiation code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 finalLines = []
-with open('input.txt', 'r') as file:
+with open('input.txt') as file:
     lines = file.readlines()
+    print(lines)
     for i in range(len(lines)):
+        print(i)
         lines[i] = lines[i].replace('\n', ' ')
         finalLines.append(lines[i].split())
 
 print(finalLines)
-with open('output.txt', 'w') as file:
-    pass
+# with open('output.txt', 'w') as file:
+#     pass
 
