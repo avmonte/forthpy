9cd2e1b2f192303fe5b5571f1d1b79454e789502
avmonte <unstoppablehay@gmail.com>
2022-12-13 02:23:36 +0400

    basic logic build, asm code correction pending

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+import numpy
+
+# init
+with open('output.txt', 'w') as file:
+    file.write('_start:\n')
+
 finalLines = []
 with open('input.txt') as file:
     lines = file.readlines()
-    print(lines)
     for i in range(len(lines)):
-        print(i)
         lines[i] = lines[i].replace('\n', ' ')
         finalLines.append(lines[i].split())
 
+finalLines = list(numpy.concatenate(finalLines).flat)
 print(finalLines)
-# with open('output.txt', 'w') as file:
-#     pass
 
+
+def add(com):
+    with open('output.txt', 'a') as file:
+        file.write(com)
+
+
+for i in finalLines:
+    if str(i).isdigit():
+        add('\tpush ' + i + '\n')
+    elif str(i) in '+-*':
+        pass
+    elif str(i) == 'dup':
+        add('\tpush %rax\n')
+    elif str(i) == 'swap':
+        add('\tpop %rax, %rdx\n\tpop %rax, %rcx\n\tpush $rdx\n\tpush %rcx\n')
+
+# exit
+add('\tret\n')
