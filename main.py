41fbf02919a67f3366c7706da156b3e1c30aec75
avmonte <unstoppablehay@gmail.com>
2022-12-13 02:35:58 +0400

    updated logic

--- main.py
+++ main.py
@@ -17,14 +17,18 @@ print(finalLines)
 
 def add(com):
     with open('output.txt', 'a') as file:
-        file.write(com)
+        file.write(com + '\n')
 
 
 for i in finalLines:
     if str(i).isdigit():
         add('\tpush ' + i + '\n')
-    elif str(i) in '+-*':
-        pass
+    elif str(i) in '+':
+        add('\tpop %rax, %rdx\n\tpop %rax, %rcx\n\tadd $rdx, %rcx\n\tpush %rcx\n')
+    elif str(i) in '-':
+        add('\tpop %rax, %rdx\n\tpop %rax, %rcx\n\tsub $rdx, %rcx\n\tpush %rcx\n')
+    elif str(i) in '*':
+        add('\tpop %rax, %rdx\n\tpop %rax, %rcx\n\timul $rdx, %rcx\n\tpush %rcx\n')
     elif str(i) == 'dup':
         add('\tpush %rax\n')
     elif str(i) == 'swap':
