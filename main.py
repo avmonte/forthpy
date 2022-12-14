db69423d5e6e80188dedb4306e31d4f4131bb7d8
avmonte <unstoppablehay@gmail.com>
2022-12-14 20:04:43 +0400

    '/' (division) and 'drop' commands added
    potential bug identified

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import sys
-
 import numpy
 import os
 
-# TODO sys input
-
+# command-line run with 1 argument
 inputFile = sys.argv[1]
 outputFile = inputFile.split('.')[0] + '.s'
 
-# init
+# output file init
 with open(outputFile, 'w') as file:
     file.write('\n.global _start\n\n_start:\n')
 
@@ -38,6 +36,8 @@ def opers():
             add('\tsub %rdx, %rcx\n', endline='')
         case '*':
             add('\timul %rdx, %rcx\n', endline='')
+        case '/':
+            add('\tdiv %rdx, %rcx\n', endline='')
 
     add('\tpush %rcx\n')
 
@@ -47,15 +47,22 @@ for i in finalLines:
         add('\tpush $' + i + '\n')
     elif str(i) in '+-*':
         opers()
-    elif str(i) == 'dup':
-        add('\tpop %rdx\n'
-            '\tpush %rdx\n'
-            '\tpush %rdx\n')
-    elif str(i) == 'swap':
-        add('\tpop %rdx\n'
-            '\tpop %rcx\n'
-            '\tpush %rdx\n'
-            '\tpush %rcx\n')
+    else:
+        match str(i):
+            case 'dup':
+                add('\tpop %rdx\n'
+                    '\tpush %rdx\n'
+                    '\tpush %rdx\n')
+            case 'swap':
+                add('\tpop %rdx\n'
+                    '\tpop %rcx\n'
+                    '\tpush %rdx\n'
+                    '\tpush %rcx\n')
+            case 'drop':
+                add('\tpop %rdx\n')
+
+
+            # TODO check if any conflicts between operations use of registers
 
 # TODO print : .s
 
